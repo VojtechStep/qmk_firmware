4d07cc0369093b49719ce1f94c39e58c18aa64a1
VojtechStep <vojtechstepancik@outlook.com>
2024-11-05 17:59:08 +0100

    Remove wally-cli from nix shell
    Replaced with running `qmk flash`

--- default-shell.nix
+++ default-shell.nix
@@ -1,8 +1,1 @@
-let
-  sources = import ./util/nix/sources.nix;
-  nixpkgs = import sources.nixpkgs { };
-in
-(import ./shell.nix { arm = false; }).overrideAttrs
-  (oldAttrs: {
-    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ nixpkgs.wally-cli ];
-  })
+import ./shell.nix { arm = false; }
